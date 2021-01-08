cf90589c5c02cfb77321c5d3dc8c46a5713b1e03
Galo Sandoval <galosan92@gmail.com>
2021-01-07 16:34:12 -0800

    adds styling to message component

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,18 +1,21 @@
-import React from "react"
-import './Message.css'
+import React from "react";
+import "./Message.css";
 
-const Message = ({message, timestamp, user, userImg}) => {
+const Message = ({ message, timestamp, user, userImg }) => {
   return (
     <div className="message">
       <img src={userImg} alt="" />
       <div className="message__info">
         <h4>
-          {user}<span className="message__timestamp">{new Date(timestamp?.toDate()).toUTCString()}</span> {new Date(timestamp?.toDate()).toUTCString()}
+          {user}{" "}
+          <span className="message__timestamp">
+            {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
         </h4>
         <p>{message}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
